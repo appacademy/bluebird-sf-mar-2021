563d2f49caf45b3cf13562bccdc88c6260e1b368
Erik Elliott <erikelliott92@gmail.com>
2021-04-16 08:46:40 -0700

    Add AR skeleton

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -34,5 +34,18 @@ class Chirp < ApplicationRecord
     has_many :likers,
         through: :likes,
         source: :liker
+    
+        
+    #Find all chirps for a particular user
+
+    #Find all chirps liked by people politically affiliated with JavaScript
+
+    #Get only the unique values from the previous query
+
+    #Find all chirps with no likes
+
+    #Find how many likes each chirp has
+
+    #Find chirps with at least 3 likes
 
 end
