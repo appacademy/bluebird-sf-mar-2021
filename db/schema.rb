e6729034031d0e5655059d7f6f36bd1734b74c1a
Julia Wang <juliawang105@gmail.com>
2021-04-25 18:16:32 -0700

    Configure for auth lecture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_180722) do
+ActiveRecord::Schema.define(version: 2021_04_26_011509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,11 @@ ActiveRecord::Schema.define(version: 2021_04_15_180722) do
     t.string "email", null: false
     t.integer "age", null: false
     t.string "political_affiliation", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["political_affiliation"], name: "index_users_on_political_affiliation"
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
